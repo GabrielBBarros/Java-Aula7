0f80ea50c8c9ce41ebdd35cc9e615f9f69f0aac0
Gabriel Bevilacqua Barros <92558878+GabrielBBarros@users.noreply.github.com>
2023-04-24 20:07:36 -0300

    Add files via upload

---
+++ Banco/Cliente.java
@@ -0,0 +1,18 @@
+package Banco;
+
+public class Cliente {
+	public int id;
+
+	public Cliente(int id) {
+		this.id = id;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+}
